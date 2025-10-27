c95d19c6bc1868e520f0b294de08826e0681a45b
dewy20 <203051065+dewy20@users.noreply.github.com>
2025-10-27 22:35:17 +0800

    hari ini

--- config/class-Kariyawan.php
+++ config/class-Kariyawan.php
@@ -3,39 +3,37 @@
 // Memasukkan file konfigurasi database
   include_once 'db-config.php';
 
-  class Kariyawan extends Database {
+  class produk extends Database {
 
     // Method untuk input data mahasiswa
-    public function inputKariyawan($data){
+    public function inputproduk($data){
         // Mengambil data dari parameter $data
-        $nik      = $data['nik'];
+        $nik      = $data['id_produk'];
         $nama     = $data['nama'];
-        $jabatan  = $data['jabatan'];
-        $alamat   = $data['alamat'];
-        $provinsi = $data['provinsi'];
-        $email    = $data['email'];
-        $telp     = $data['telp'];
-        $status   = $data['status'];
+        $jabatan  = $data['harga'];
+        $alamat   = $data['stok'];
+        
         // Menyiapkan query SQL untuk insert data menggunakan prepared statement
-        $query = "INSERT INTO tb_Kariyawan (nik_kyw, nama_kyw, jabatan_kyw, alamat, provinsi, email, telp, status_kyw) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $query = "INSERT INTO tb_produk (id_produk, nama_produk, harga_produk, stok_produk,  VALUES (?, ?, ?, ?)";
         $stmt = $this->conn->prepare($query);
         if(!$stmt){
             return false;
         }
         // Memasukkan parameter ke statement
-        $stmt->bind_param("ssssssss", $nik, $nama, $jabatan, $alamat, $provinsi, $email, $telp, $status);
+        $stmt->bind_param("ssssssss", $id, $nama, $harga, $stok);
         $result = $stmt->execute();
         $stmt->close();
         return $result;
     }
-
-    // Method untuk mengambil semua data kariyawan
-    public function getAllKariyawan(){
-        // Menyiapkan query SQL untuk mengambil data Kariyawan beserta prodi dan provinsi
-        $query = "SELECT id_kyw, nik_kyw, nama_kyw, nama_jabatan, nama_provinsi, alamat, email, telp, status_kyw
-              FROM tb_Kariyawan
-              JOIN tb_jabatan ON jabatan_kyw = id_jabatan
-              JOIN tb_provinsi ON provinsi = id_provinsi";
+       
+}
+    // Method untuk mengambil semua data produk
+    public function getAllproduk(){
+        // Menyiapkan query SQL untuk mengambil data produk beserta prodi dan provinsi
+        $query = "SELECT id_produk, nama_produk, harga_produk, stok_produk
+              FROM tb_produk
+              JOIN tb_produk ON nama_produk = id_produk"{
+              
         $result = $this->conn->query($query);
         // Menyiapkan array kosong untuk menyimpan data Kariyawan 
         $Kariyawan = [];
@@ -43,24 +41,19 @@
         if($result->num_rows > 0){ 
             // Mengambil setiap baris data dan memasukkannya ke dalam array
             $Kariyawan[] = [ 
-                'id' => $row['id_kyw'],
-                'nik' => $row['nik_kyw'],
-                'nama' => $row['nama_kyw'],
-                'jabatan' => $row['nama_jabatan'],
-                'provinsi' => $row['nama_provinsi'],
-                'alamat' => $row['alamat'],
-                'email' => $row['email'],
-                'telp' => $row['telp'],
-                'status' => $row['status_kyw']
+                'id' => $row['id_produk'],
+                'nama' => $row['nama_produk'],
+                'harga' => $row['harga_produk'],
+                'stok' => $row['stok_produk'],
             ];
         }
-        return $Kariyawan;
+        return $produk;
     }
-
+        
     // Method untuk mengambil data mahasiswa berdasarkan ID
-    public function getUpdateKariyawan($id){
+    public function getUpdateproduk($id){
         // Menyiapkan query SQL untuk mengambil data kariyawan berdasarkan ID menggunakan prepared statement
-        $query = "SELECT * FROM tb_Kariyawan WHERE id_kyw = ?";
+        $query = "SELECT * FROM tb_produk WHERE id_produk = ?";
         $stmt = $this->conn->prepare($query);
         if(!$stmt){
             return false;
@@ -74,15 +67,11 @@
             $row = $result->fetch_assoc();
             // Menyimpan data dalam array
             $data = [
-                'id' => $row['id_kyw'],
-                'nik' => $row['nik_kyw'],
-                'nama' => $row['nama_kyw'],
-                'jabatan' => $row['jabatan_kyw'],
-                'alamat' => $row['alamat'],
-                'provinsi' => $row['provinsi'],
-                'email' => $row['email'],
-                'telp' => $row['telp'],
-                'status' => $row['status_kyw']
+                'id' => $row['id_produk'],
+                'nik' => $row['nama_produk'],
+                'nama' => $row['harga_produk'],
+                'jabatan' => $row['stok_produk'],
+                
             ];
         }
         $stmt->close();
@@ -90,33 +79,30 @@
     }
 
     // Method untuk mengedit data kariyawan
-    public function editKariyawan($data){
+    public function editproduk($data){
         // Mengambil data dari parameter $data
-         $nik      = $data['nik'];
+         $nik      = $data['id_produk'];
         $nama     = $data['nama'];
-        $jabatan  = $data['jabatan'];
-        $alamat   = $data['alamat'];
-        $provinsi = $data['provinsi'];
-        $email    = $data['email'];
-        $telp     = $data['telp'];
-        $status   = $data['status'];
+        $harga  = $data['harga'];
+        $stok   = $data['stok'];
+        
         // Menyiapkan query SQL untuk update data menggunakan prepared statement
-        $query = "UPDATE tb_Kariyawan SET nik_kyw = ?, nama_kyw = ?, jabatan_kyw = ?, alamat = ?, provinsi = ?, email = ?, telp = ?, status_kyw = ? WHERE id_kyw = ?";
+        $query = "UPDATE tb_produk SET id_produk = ?, nama_produk = ?, harga_produk = ?, stok  = ? WHERE id_produk = ?";
         $stmt = $this->conn->prepare($query);
         if(!$stmt){
             return false;
         }
         // Memasukkan parameter ke statement
-        $query = "UPDATE tb_Kariyawan SET nik_kyw = ?, nama_kyw = ?, jabatan_kyw = ?, alamat = ?, provinsi = ?, email = ?, telp = ?, status_kyw = ? WHERE id_kyw = ?";
+        $query = "UPDATE tb_produk SET id_produk = ?, nama_produk = ?, harga_produk = ?, stok  = ? WHERE id_produk = ?";
         $stmt->close();
         // Mengembalikan hasil eksekusi query
         return $result;
     }
 
     // Method untuk menghapus data mahasiswa
-    public function deleteKariyawan($id){
+    public function deleteproduk($id){
         // Menyiapkan query SQL untuk delete data menggunakan prepared statement
-        $query = "DELETE FROM tb_Kariyawan WHERE id_kyw = ?";
+        $query = "DELETE FROM tb_produk WHERE id_produk = ?";
         $stmt = $this->conn->prepare($query);
         if(!$stmt){
             return false;
@@ -129,15 +115,13 @@
     }
 
     // Method untuk mencari data mahasiswa berdasarkan kata kunci
-    public function searchKariyawan($kataKunci){
+    public function searchproduk($kataKunci){
         // Menyiapkan LIKE query untuk pencarian
         $likeQuery = "%".$kataKunci."%";
         // Menyiapkan query SQL untuk pencarian data mahasiswa menggunakan prepared statement
-        $query = "SELECT id_kyw, nik_kyw, nama_kyw, nama_jabatan, nama_provinsi, alamat, email, telp, status_kyw 
-                  FROM tb_kariyawan
-                  JOIN tb_jabatan ON jabatan_kyw = id_jabatan
-                  JOIN tb_provinsi ON provinsi = id_provinsi
-                  WHERE nik_kyw LIKE ? OR nama_kyw LIKE ?";
+        $query = "SELECT id_produk, nama_produk, harga_produk, stok_produk
+                  JOIN tb_kategory ON nama_produk = id_produk
+                  WHERE id_produk LIKE ? OR nama_produk LIKE ?";
         $stmt = $this->conn->prepare($query);
         if(!$stmt){
             // Mengembalikan array kosong jika statement gagal disiapkan
@@ -154,15 +138,11 @@
             while($row = $result->fetch_assoc()) {
                 // Menyimpan data Kariyawan dalam array
                 $Kariyawan[] = [
-                    'id' => $row['id_kyw'],
-                    'nik' => $row['nik_kyw'],
-                    'nama' => $row['nama_kyw'],
-                    'jabatan' => $row['nama_jabatan'],
-                    'provinsi' => $row['nama_provinsi'],
-                    'alamat' => $row['alamat'],
-                    'email' => $row['email'],
-                    'telp' => $row['telp'],
-                    'status' => $row['status_kyw']
+                    'id' => $row['id_produk'],
+                    'nama' => $row['nama_produk'],
+                    'harga' => $row['harga_produk'],
+                    'stok' => $row['stok_produk'],
+                    
                 ];
             }
         }
--- db_simplecrud.sql
+++ db_simplecrud.sql
@@ -20,55 +20,29 @@ CREATE DATABASE IF NOT EXISTS `db_simplecrud` /*!40100 DEFAULT CHARACTER SET utf
 USE `db_simplecrud`;
 
 -- Tabel karyawan (pengganti tb_Kariyawan)
-CREATE TABLE IF NOT EXISTS `tb_Kariyawan` (
-  `id_kyw` int(11) NOT NULL AUTO_INCREMENT,
-  `nik_kyw` char(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nama_kyw` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `jabatan_kyw` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `provinsi` smallint(3) NOT NULL,
-  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `telp` char(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `status_kyw` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_kyw`)
+CREATE TABLE IF NOT EXISTS `tb_produk` (
+  `id_produk` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_produk` char(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga_produk` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stok` text COLLATE utf8mb4_unicode_ci NOT NULL,
+ 
+  PRIMARY KEY (`id_produk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table db_simplecrud.tb_mahasiswa: ~0 rows (approximately)
 
 -- Dumping structure for table db_simplecrud.tb_prodi
-CREATE TABLE IF NOT EXISTS `tb_jabatan` (
-  `id_jabatan` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nama_jabatan` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_jabatan`)
+CREATE TABLE IF NOT EXISTS `tb_kategory` (
+  `id_produk` char(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama_produk` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id_produk`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Dumping data for table db_simplecrud.tb_prodi: ~9 rows (approximately)
-INSERT INTO `tb_jabatan` (`id_jabatan`, `nama_jabatan`) VALUES  ###jabatan kariayawan
-('ADM', 'Admin'),
-('KSR', 'Kasir'),
-('HRD', 'HRD'),
-('MNG', 'Manajer'),
-('SPV', 'Supervisor'),
-('IT', 'IT Support');
+INSERT INTO `tb_produk` (`id_produk`, `nama_produk`) VALUES  ###nama_produk kariayawan
+('123', 'sirih putih'),
+('456', 'sirih hitam'),
+('789', 'tunas sirih'),
+('10012', 'sirih wangi'),
+('48210', 'sirih daun'),
 
--- Dumping structure for table db_simplecrud.tb_provinsi
-CREATE TABLE IF NOT EXISTS `tb_provinsi` (
-  `id_provinsi` smallint(3) NOT NULL AUTO_INCREMENT,
-  `nama_provinsi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_provinsi`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Dumping data for table db_simplecrud.tb_provinsi: ~6 rows (approximately)
-INSERT INTO `tb_provinsi` (`id_provinsi`, `nama_provinsi`) VALUES
-	(1, 'Bali'),
-	(2, 'Nusa Tenggara Timur'),
-	(3, 'Nusa Tenggara Barat'),
-	(4, 'Jawa Timur'),
-	(5, 'Jawa Tengah'),
-	(6, 'Jawa Barat');
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
