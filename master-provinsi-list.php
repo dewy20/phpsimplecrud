0eac929ca4f37e455e5eceb673beb187ad5fb0fb
dewy20 <203051065+dewy20@users.noreply.github.com>
2025-11-01 00:33:08 +0800

    hari ini

--- master-provinsi-list.php
+++ master-provinsi-list.php
@@ -14,7 +14,7 @@ if(isset($_GET['status'])){
 		echo "<script>alert('Gagal menghapus data provinsi. Silakan coba lagi.');</script>";
 	}
 }
-$dataProvinsi = $master->getProvinsi();
+$datakategory = $master->getkategory();
 
 ?>
 <!doctype html>
@@ -37,12 +37,12 @@ $dataProvinsi = $master->getProvinsi();
 					<div class="container-fluid">
 						<div class="row">
 							<div class="col-sm-6">
-								<h3 class="mb-0">Data Provinsi</h3>
+								<h3 class="mb-0">Data kategory</h3>
 							</div>
 							<div class="col-sm-6">
 								<ol class="breadcrumb float-sm-end">
 									<li class="breadcrumb-item"><a href="index.php">Beranda</a></li>
-									<li class="breadcrumb-item active" aria-current="page">Master Provinsi</li>
+									<li class="breadcrumb-item active" aria-current="page">Master kategory</li>
 								</ol>
 							</div>
 						</div>
@@ -55,7 +55,7 @@ $dataProvinsi = $master->getProvinsi();
 							<div class="col-12">
 								<div class="card">
 									<div class="card-header">
-										<h3 class="card-title">Daftar Provinsi</h3>
+										<h3 class="card-title">Daftar kategory</h3>
 										<div class="card-tools">
 											<button type="button" class="btn btn-tool" data-lte-toggle="card-collapse" title="Collapse">
 												<i data-lte-icon="expand" class="bi bi-plus-lg"></i>
@@ -77,18 +77,18 @@ $dataProvinsi = $master->getProvinsi();
 											</thead>
 											<tbody>
 												<?php
-													if(count($dataProvinsi) == 0){
+													if(count($datakategory) == 0){
 													    echo '<tr class="align-middle">
 															<td colspan="3" class="text-center">Tidak ada data provinsi.</td>
 														</tr>';
 													} else {
-														foreach ($dataProvinsi as $index => $provinsi){
+														foreach ($datakategory as $index => $datakategory){
 															echo '<tr class="align-middle">
 																<td>'.($index + 1).'</td>
-																<td>'.$provinsi['nama'].'</td>
+																<td>'.$datakategory['nama'].'</td>
 																<td class="text-center">
-																	<button type="button" class="btn btn-sm btn-warning me-1" onclick="window.location.href=\'master-provinsi-edit.php?id='.$provinsi['id'].'\'"><i class="bi bi-pencil-fill"></i> Edit</button>
-																	<button type="button" class="btn btn-sm btn-danger" onclick="if(confirm(\'Yakin ingin menghapus data program studi ini?\')){window.location.href=\'proses/proses-provinsi.php?aksi=deleteprovinsi&id='.$provinsi['id'].'\'}"><i class="bi bi-trash-fill"></i> Hapus</button>
+																	<button type="button" class="btn btn-sm btn-warning me-1" onclick="window.location.href=\'master-provinsi-edit.php?id='.$datakategory['id'].'\'"><i class="bi bi-pencil-fill"></i> Edit</button>
+																	<button type="button" class="btn btn-sm btn-danger" onclick="if(confirm(\'Yakin ingin menghapus data program studi ini?\')){window.location.href=\'proses/proses-provinsi.php?aksi=deleteprovinsi&id='.$datakategory['id'].'\'}"><i class="bi bi-trash-fill"></i> Hapus</button>
 																</td>
 															</tr>';
 														}
@@ -98,7 +98,7 @@ $dataProvinsi = $master->getProvinsi();
 										</table>
 									</div>
 									<div class="card-footer">
-										<button type="button" class="btn btn-primary" onclick="window.location.href='master-provinsi-input.php'"><i class="bi bi-plus-lg"></i> Tambah Provinsi</button>
+										<button type="button" class="btn btn-primary" onclick="window.location.href='master-provinsi-input.php'"><i class="bi bi-plus-lg"></i> Tambah kategory</button>
 									</div>
 								</div>
 							</div>
